6e2802a7309f2d10cb1b3c16b4a4f79703cee134
Lindsay Hammon <lindsay.work.mail@gmail.com>
2014-12-26 14:52:28 -0700

    fixed a bug were it dident add fizbuz

--- fizbuz.rb
+++ fizbuz.rb
@@ -2,9 +2,9 @@ numbers = (1..100).to_a
 fiz = "Fiz"
 buz = "buz"
 fizbuz = "fizbuz"
-numbers.map! { |num| num % 3 == 0 ? fiz : num }.flatten!
+numbers.map! { |num| num % 3 == 0 && num % 5 == 0  ? fizbuz : num }
+numbers.map! { |num| num % 3 == 0 ? fiz : num }
 numbers.map! { |num| num % 5 == 0 ? buz : num }.flatten!
-numbers.map! { |num| num % 3 == 0 && num % 5 == 0  ? fiz : num }.flatten!
 
 puts numbers
 
